5af849f841bf7ff3a68b9c9cd0ce8a8142acb682
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-03-02 14:37:59 -0300

    função doWhile

---
+++ src/main/kotlin/do_while.kt
@@ -0,0 +1,11 @@
+fun doWhile(to: Int) {
+    var test = true
+    var count = 0
+    do {
+        println(count)
+        count++
+        if (count >= to) {
+            test = false
+        }
+    } while(test)
+}
